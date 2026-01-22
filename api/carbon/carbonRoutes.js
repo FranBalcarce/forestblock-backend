7c4f622962b01574a6a21e99932663611841c5ff
Your Name~Francisco <franbalcarce21@gmail.com>
2026-01-22 10:28:24 -0300

    fix

--- api/carbon/carbonRoutes.js
+++ api/carbon/carbonRoutes.js
@@ -1,28 +1,12 @@
 const express = require("express");
-const {
-  generateQuote,
-  createOrder,
-  getOrderDetails,
-  sharePdf,
-  getCarbonProjects,
-  getCarbonProjectById,
-  getPrices,
-  pollOrderStatus,
-} = require("./carbonController");
+
+const { getCarbonProjects, getPrices } = require("./carbonController");
 
 const router = express.Router();
 
+// 🔥 ESTAS FUNCIONES TIENEN QUE EXISTIR
 router.get("/carbonProjects", getCarbonProjects);
-router.get("/carbonProjects/:id", getCarbonProjectById);
-
 router.get("/prices", getPrices);
-
-router.get("/orders/:orderId", getOrderDetails);
-router.get("/orders", pollOrderStatus);
-
-router.post("/generate-quote", generateQuote);
-router.post("/create-order", createOrder);
-router.post("/sharePDF", sharePdf);
 
 module.exports = router;
 
