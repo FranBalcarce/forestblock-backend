616fd30cf09120621a66de12ace8589a3a684f0d
Your Name~Francisco <franbalcarce21@gmail.com>
2026-01-26 09:57:27 -0300

    fixing prices

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import express from "express";
+const express = require("express");
 
-import authRoutes from "./auth/authRoutes.js";
-import carbonRoutes from "./carbon/carbonRoutes.js";
+const authRoutes = require("./auth/authRoutes");
+const userRoutes = require("./users/userRoutes");
+const paymentRoutes = require("./payments/paymentRoutes");
+const carbonRoutes = require("./carbon/carbonRoutes");
+const manglaiRoutes = require("./manglai/manglaiRoutes");
+const retirementsRoutes = require("./retirements/retirementsRoutes");
+const klimapiRoutes = require("./klimapi/klimapiRoutes");
+const planInquiryRoutes = require("./plan-inquiry/planRoutes");
 
 const router = express.Router();
 
@@ -10,6 +16,12 @@ router.get("/", (_, res) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/payments", paymentRoutes);
 router.use("/carbon", carbonRoutes);
+router.use("/manglai", manglaiRoutes);
+router.use("/retirements", retirementsRoutes);
+router.use("/klimapi", klimapiRoutes);
+router.use("/plan", planInquiryRoutes);
 
-export default router;
+module.exports = router;
