bd2f8eeaebcda001830c7a51892bdfb6b438f02d
Your Name~Francisco <franbalcarce21@gmail.com>
2026-01-26 11:15:45 -0300

    arregglos

--- api/utils/encryption.js
+++ api/utils/encryption.js
@@ -1,16 +1,36 @@
-const crypto = require("crypto");
+import crypto from "crypto";
 
-function encryptPrivateKey(privateKey, passphrase) {
-  // Derivar una clave de 32 bytes usando SHA-256
-  const key = crypto.createHash("sha256").update(passphrase).digest();
+const ALGORITHM = "aes-256-cbc";
 
-  const iv = crypto.randomBytes(16); // Generar IV aleatorio
-  const cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
+export const encryptPrivateKey = (text, secret) => {
+  const iv = crypto.randomBytes(16);
 
-  let encryptedData = cipher.update(privateKey, "utf8", "hex");
-  encryptedData += cipher.final("hex");
+  const key = crypto.createHash("sha256").update(secret).digest();
 
-  return { encryptedData, iv: iv.toString("hex") };
-}
+  const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
 
-module.exports = { encryptPrivateKey };
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  return {
+    encryptedData: encrypted,
+    iv: iv.toString("hex"),
+  };
+};
+
+// const crypto = require("crypto");
+
+// function encryptPrivateKey(privateKey, passphrase) {
+//   // Derivar una clave de 32 bytes usando SHA-256
+//   const key = crypto.createHash("sha256").update(passphrase).digest();
+
+//   const iv = crypto.randomBytes(16); // Generar IV aleatorio
+//   const cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
+
+//   let encryptedData = cipher.update(privateKey, "utf8", "hex");
+//   encryptedData += cipher.final("hex");
+
+//   return { encryptedData, iv: iv.toString("hex") };
+// }
+
+// module.exports = { encryptPrivateKey };
